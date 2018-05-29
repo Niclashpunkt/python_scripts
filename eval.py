b99816c1dcf92f0690430f6b14e340861d61073c
niclashpunkt <n.hedemann@posteo.de>
2018-05-29 12:53:07 +0200

    initial commit

---
+++ eval.py
@@ -0,0 +1,13 @@
+
+def eval(inputStr, baseStr):
+
+    if inputStr in baseStr:
+        print("Yes! \(^.^)/")
+
+    else:
+        print("Sadly not (-_-)")
+
+
+# strA = "Alpha"
+# strB = "Alpha Beta Gamma"
+# evalList(strA, strB)
